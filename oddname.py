524e2c8a99f8a34288c60ec943badeec3416830a
limengyuan <mengyuan.li@my.jcu.edu.au>
2018-11-20 09:45:04 +0800

    add commit

---
+++ oddname.py
@@ -0,0 +1,10 @@
+name = input("Enter name:-")
+
+while len(name)<=0:
+    print("Name is blank, enter again ")
+    name = input("Enter name:-")
+
+print(name[::2])
+
+# for i in range(0,len(name), 2)
+#     print(name[i], end='')
